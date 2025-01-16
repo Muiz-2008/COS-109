e525051b51230a85ffba1c1796723a31ff878045
Muiz-2008 <afolabiabdulmuiz5@gmail.com>
2025-01-16 15:20:27 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Simple form submission message
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for your message! I really appreciate your feedback.");
-    document.getElementById("contactForm").reset();
-});
